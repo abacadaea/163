e2fdd75a3496d591682c3bf0f8225556b990a20c
EC2 Default User <ec2-user@ip-172-31-46-202.us-west-2.compute.internal>
2014-08-10 14:05:18 +0000

    modified build script

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-export PYTHON_PATH=~/Websites/163/src
-./server.py
+mkdir -p /tmp/163
+PID_FILE=/tmp/163/163.pid
+LOG_FILE=/tmp/163/163.log
+SOCKET=/tmp/163/163.socket
+
+export PYTHON_PATH=~/Websites/163/src:~/163/src
+
+uwsgi --stop $PID_FILE
+uwsgi -s $SOCKET --module server --callable app --chmod-socket=666 --pidfile $PID_FILE --logto $LOG_FILE &
